9687e08018863d45d00f29700774346a14086d9c
Kakoha <dhiamok1@gmail.com>
2023-02-04 11:35:46 +0100

    hjig

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public Unit unit;
+    public bool canGoUp;
     public bool filled { get { return unit == null; } }
     private void Update()
     {
-        if(this == GameManager.instance.currentHit)
+        if(canGoUp)
         {
-            goUp();
-        }
-        else
-        {
-            goDown();
+            if (this == GameManager.instance.currentHit)
+            {
+                goUp();
+            }
+            else
+            {
+                goDown();
+            }
         }
     }
-    public void goUp()
+    public IEnumerator goUp()
     {
-        transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        while(transform.position.y < 0.5)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y + 0.5f*Time.deltaTime, transform.position.z);
+            yield return new WaitForEndOfFrame();
+        }
     }
     public void goDown()
     {
