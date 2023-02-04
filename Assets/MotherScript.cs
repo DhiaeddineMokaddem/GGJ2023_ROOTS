9c9f1d75da9f9c73489114b3ffd86130be512a87
haithemelhadj <haithemelhadj42@gmail.com>
2023-02-04 13:40:14 +0100

    Update MotherScript.cs

--- Assets/MotherScript.cs
+++ Assets/MotherScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MotherScript
+public class MotherScript: MonoBehaviour
 {
     //define this class variables here (health,level)
     public int health;
