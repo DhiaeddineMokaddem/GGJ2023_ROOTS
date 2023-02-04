9e65058e5c3f9ab5ed351a7cc75414e1725f5434
Kakoha <dhiamok1@gmail.com>
2023-02-04 11:46:19 +0100

    comments

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public Unit unit;
-    public bool canGoUp;
-    public bool filled { get { return unit == null; } }
+    public Unit unit; //the unit that occupies this tile
+    public bool canGoUp; //if the tile can go up when hovered over
+    public bool filled { get { return unit == null; } } //checks if the tile has a unit on it
     private void Update()
     {
         if(canGoUp)
         {
-            if (this == GameManager.instance.currentHit)
+            if (this == GameManager.instance.currentHit) //checks if itself is the tile that is being hovered over
             {
-                goUp();
+                StartCoroutine(goUp()); //goes up if so
             }
             else
             {
-                goDown();
+                goDown(); //stays down or goes back down (unoptemized)
             }
         }
     }
-    public IEnumerator goUp()
+    public IEnumerator goUp() //inprogress
     {
-        while(transform.position.y < 0.5)
+        StopCoroutine("goUp");
+        while (transform.position.y < 0.5)
         {
             transform.position = new Vector3(transform.position.x,transform.position.y + 0.5f*Time.deltaTime, transform.position.z);
             yield return new WaitForEndOfFrame();
         }
     }
-    public void goDown()
+    public void goDown() //inprogress
     {
         transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
